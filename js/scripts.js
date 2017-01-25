1b19213f4e854afd4501819f972cfda7abf700ba
Kamil Dziuba <kamil.dziuba@gmail.com>
2017-01-25 13:33:37 +0100

    poprawka js

--- js/scripts.js
+++ js/scripts.js
@@ -1,5 +1,6 @@
-// plik scripts.js
 
-var name = prompt('Jak się nazywasz?');
-alert('Twoje imię to: ' + name);
-console.log('Witaj, ' + name);
+var a = 15,
+    h = 20;
+var triangleArea = a*h/2;
+console.log('Pole trójkąta o podstawie a: ' + a + ' i wysokości h: ' + h + ' wynosi: ' + triangleArea);
+prompt ('Pole o podstawie a i wysokości h, wynosi:', triangleArea)
