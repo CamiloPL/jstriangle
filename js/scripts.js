3a84dca97287ad1e094dc01b5482f0e65971e06e
Nemethe <nowakmarceli@gmail.com>
2017-01-25 13:48:00 +0100

    zmiana prompt na alert

--- js/scripts.js
+++ js/scripts.js
@@ -3,4 +3,4 @@ var a = 15,
     h = 20;
 var triangleArea = a*h/2;
 console.log('Pole trójkąta o podstawie a: ' + a + ' i wysokości h: ' + h + ' wynosi: ' + triangleArea);
-prompt ('Pole o podstawie a i wysokości h, wynosi:', triangleArea)
+alert('Pole o podstawie a: ' + a + ' i wysokości h: ' + h + ', wynosi: ' + triangleArea);
